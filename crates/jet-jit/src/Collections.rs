92fc30f35d92e974ce82bbaea4fe470fe295c497
HalcyonOmega <nathanbrown@me.com>
2026-09-12 12:49:57 -0400

    fix(jit): alias BitSet constructor

--- crates/jet-jit/src/Collections.rs
+++ crates/jet-jit/src/Collections.rs
@@ -10441,6 +10441,7 @@ host_fns! {
     lru_keys: "jet_jit_lru_keys" => jet_jit_lru_keys: sig_len;
     checked_lru_keys: "jet_lru_keys" => jet_jit_lru_keys: sig_len;
     bit_set_new: "jet_jit_bit_set_new" => jet_jit_bit_set_new: sig_new;
+    checked_bit_set_new: "JetBitSet::new" => jet_jit_bit_set_new: sig_new;
     bit_set_add: "jet_jit_bit_set_add" => jet_jit_bit_set_add: sig_list_eq;
     bit_set_has: "jet_jit_bit_set_has" => jet_jit_bit_set_has: sig_list_eq;
     checked_bit_set_has: "jet_bit_set_has" => jet_jit_bit_set_has: sig_list_eq;
